2b4a4f9dbe8a644426a658ffe5b3cdc1ebb0bbff
Jack Casey <25445014+jjkc33@users.noreply.github.com>
2021-11-11 15:16:39 -0800

    first pass at conv modules

--- utils/pp_modules.py
+++ utils/pp_modules.py
@@ -67,3 +67,83 @@ class MLP(nn.Module):
                 inputs = F.relu(inputs)
 
         return inputs
+
+
+class PowerPropConv(nn.Module):
+    """Powerpropagation Conv2D module."""
+
+    def __init__(self, alpha, in_channels, out_channels, kernel_size=3):
+        super(PowerPropConv, self).__init__()
+        self.alpha = alpha
+        self.w = torch.nn.Parameter(torch.empty(out_channels, in_channels, kernel_size, kernel_size))
+        self.b = torch.nn.Parameter(torch.empty(out_channels))
+
+    def get_weights(self):
+        return torch.sign(self.w) * torch.pow(torch.abs(self.w), self.alpha)
+
+    def forward(self, inputs, mask=None):
+
+        weights = self.w * torch.pow(torch.abs(self.w), self.alpha - 1)
+
+        if mask is not None:
+            weights *= mask
+
+        outputs = F.conv2d(inputs, weights, self.b)
+
+        return outputs
+
+
+class CNN(nn.Module):
+    """A convolutional neural network module."""
+    def __init__(self, alpha, in_channels=3, out_channels=(64, 128, 256)):
+        super(CNN, self).__init__()
+        self.alpha = alpha
+        self.conv_layers = nn.ModuleDict()
+        self.fc_layers = nn.ModuleList()
+
+        # create convolutional layers
+        for i, channels in enumerate(out_channels):
+            self.conv_layers[f'ConvLayer{2*i + 1}'] = PowerPropConv(
+                alpha=alpha, in_channels=in_channels, out_channels=channels
+            )
+            self.conv_layers[f'ConvLayer{2*i + 2}'] = PowerPropConv(
+                alpha=alpha, in_channels=channels, out_channels=channels
+            )
+            in_channels = channels
+
+        # create FC layers
+        in_features = 512
+        for i, features in enumerate((256, 256, 10)):
+            self.fc_layers.append(
+                PowerPropLinear(alpha=alpha, in_features=in_features, out_features=features)
+            )
+            in_features = features
+
+    def get_weights(self):
+        return [layer.get_weights().numpy() for layer in self._layers.values()]
+
+    def forward(self, inputs, masks=None):
+        for i in range(len(self.conv_layers)):
+            layer = self._layers[f'ConvLayer{i + 1}']
+            if masks is not None:
+                inputs = layer(inputs, masks[i])
+            else:
+                inputs = layer(inputs)
+
+            # if first in conv pair, apply relu, otherwise apply max pool at end of pair
+            if i % 2 == 0:
+                inputs = F.relu(inputs)
+            else:
+                inputs = F.max_pool2d(inputs, kernel_size=2, stride=2)
+
+        num_fcs = len(self.fc_layers)
+        for i, layer in enumerate(self.fc_layers):
+            if masks is not None:
+                inputs = layer(inputs, masks[i])
+            else:
+                inputs = layer(inputs)
+
+            if i < (num_fcs - 1):
+                inputs = F.relu(inputs)
+
+        return inputs
