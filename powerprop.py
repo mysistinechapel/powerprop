8a9f3ea291c771eb26277a4c7cd308e2dbac041e
Jack Casey <25445014+jjkc33@users.noreply.github.com>
2021-11-11 13:23:45 -0800

    a single training epoch working

--- powerprop.py
+++ powerprop.py
@@ -1,51 +1,60 @@
 import torch
-import torchvision
-import torchvision.datasets as datasets
-from torch.utils.data import random_split
 
+from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader, TensorDataset
 
-#Training Configuration
-model_seed = 0  #@param
+from utils.pp_modules import MLP
+from utils.utils import init_weights, train, cat_loss, accuracy
 
-alphas = [1.0, 2.0, 3.0, 4.0, 5.0]  #@param
+# Training Configuration
+model_seed = 0  # @param
+
+alphas = [1.0, 2.0, 3.0, 4.0, 5.0]  # @param
 init_distribution = 'truncated_normal'
 init_mode = 'fan_in'
 init_scale = 1.0
 
 # Fixed values taken from the Lottery Ticket Hypothesis paper
 train_batch_size = 60
+validation_size = 5000
 num_train_steps = 50000
 learning_rate = 0.1
 report_interval = 2500
 
-#Training setup
-train_data = datasets.MNIST(
-    root = 'data',
-    train = True,
-    transform = ToTensor(),
-    download = True
+# Training setup
+train_data = MNIST(
+    root='./data/',
+    train=True,
+    download=True,
+    transform=ToTensor(),
 )
-test_data = datasets.MNIST(
-    root = 'data',
-    train = False,
-    transform = ToTensor()
+test_data = MNIST(
+    root='./data/',
+    train=False,
+    download=True,
+    transform=ToTensor()
 )
 
-# calculate size of train and validation sets. 80% train and 20% validation
-train_size = int(0.8 * len(train_data.train_data))
-valid_size = len(train_data.train_data) - train_size
+# reshape and normalize data
+train_data.data = train_data.data.flatten(start_dim=1).float() / 255.
+test_data.data = test_data.data.flatten(start_dim=1).float() / 255.
 
-#Randomly split the train and validation sets
-partial_train_data, validation_data = random_split(train_data, [train_size, valid_size])
+train_x = train_data.data[:-validation_size]
+train_y = train_data.targets[:-validation_size]
 
-train_x = partial_train_data.dataset.train_data
-train_y = partial_train_data.dataset.train_labels
-
-val_x = validation_data.dataset.train_data
-val_y = validation_data.dataset.train_labels
+# Reserve some data for a validation set
+valid_x = train_data.data[-validation_size:]
+valid_y = train_data.targets[-validation_size:]
 
 test_x = test_data.test_data
 test_y = test_data.test_labels
 
+dataloader = DataLoader(TensorDataset(train_x, train_y), batch_size=train_batch_size, shuffle=True)
 
+model = MLP(alpha=alphas[0])
+model.apply(init_weights)
+
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
+
+train(model, dataloader, optimizer, cat_loss, accuracy)
