corrected variance of weight inits

tests/test_backprop.py
@@ -61,12 +61,12 @@ class TestBackProp(unittest.TestCase):
         cls.x_torch = torch.from_numpy(cls.x)
         cls.y_torch = torch.from_numpy(cls.y)
 
+        cls.model = TorchMLP(alpha=cls.alpha)
+
+        weight_init = PowerPropVarianceScaling(cls.alpha)
+        cls.truth = DensityNetwork(MLP(alpha=cls.alpha, w_init=weight_init))
+
     def setUp(self) -> None:
-        self.model = TorchMLP(alpha=self.alpha)
-
-        weight_init = PowerPropVarianceScaling(self.alpha)
-        self.truth = DensityNetwork(MLP(alpha=self.alpha, w_init=weight_init))
-
         # initialize ground-truth model weights
         self.truth(self.x)
 

tests/test_linear.py
@@ -1,32 +1,58 @@
 import numpy as np
+import tensorflow as tf
 
 import torch
 import unittest
 
-from utils.pp_modules import PowerPropLinear
+from utils.pp_modules import PowerPropLinear as TorchLinear
+from utils.tf_modules import PowerPropVarianceScaling, PowerPropLinear as TFLinear
 from utils.utils import init_weights
 
 
 class TestLinearLayers(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.alpha = 2.0
+        cls.learning_rate = 0.1
+        cls.in_feats = 784
+        cls.out_feats = 300
+
+        x = np.load(r'./test_data/sample_x.npy')
+        cls.x = x.reshape([100, 784]).astype(np.float32) / 255.0
+
+        weight_init = PowerPropVarianceScaling(cls.alpha)
+        cls.tf_fc = TFLinear(output_size=cls.out_feats, alpha=cls.alpha, w_init=weight_init, name='TFLinear')
+        cls.torch_fc = TorchLinear(alpha=cls.alpha, in_features=cls.in_feats, out_features=cls.out_feats)
+
     def setUp(self) -> None:
-        self.alpha = 1.0
-        self.in_feats = 784
-        self.out_feats = 300
-        self.fc = PowerPropLinear(alpha=self.alpha, in_features=self.in_feats, out_features=self.out_feats)
-        self.fc.apply(init_weights)
+        self.tf_fc(tf.convert_to_tensor(self.x))
+        self.torch_fc.apply(init_weights)
 
     def test_biases(self):
         expected = np.zeros(self.out_feats)
-        np.testing.assert_array_equal(expected, self.fc.b.data.numpy())
-        self.assertEqual(self.out_feats, self.fc.b.shape)
+        np.testing.assert_array_equal(expected, self.torch_fc.b.data.numpy())
+        self.assertEqual(self.out_feats, self.torch_fc.b.shape[0])
 
     def test_weights(self):
         expected_mean = 0.0
-        actual_mean = torch.mean(self.fc.w.data).item()
+        actual_mean = self.torch_fc.w.mean().item()
         diff = (expected_mean - actual_mean) ** 2
-        self.assertAlmostEqual(expected_mean, diff)
-        self.assertTupleEqual((self.out_feats, self.in_feats), self.fc.w.shape)
+        self.assertAlmostEqual(expected_mean, diff, places=6)
+        self.assertTupleEqual((self.out_feats, self.in_feats), self.torch_fc.w.shape)
+
+    def test_weight_dist(self):
+        expected_val = 0.0
+        expected_std = self.tf_fc.w.numpy().std()
+
+        self.assertAlmostEqual(
+            expected_val, (expected_val - self.torch_fc.w.mean().item()) ** 2, places=6,
+            msg='Mean should be close to 0'
+        )
+        self.assertAlmostEqual(
+            expected_val, (expected_std - self.torch_fc.w.std().item()) ** 2, places=6,
+            msg=f'StdDev should be close to {expected_std}'
+        )
 
 
 if __name__ == '__main__':

utils/utils.py
@@ -36,7 +36,10 @@ def init_weights(module: nn.Module):
     if isinstance(module, (PowerPropLinear, PowerPropConv)):
         fan_in = calculate_fan_in(module.w.data)
 
-        std = np.sqrt(1. / fan_in)
+        # constant from scipy.stats.truncnorm.std(a=-2, b=2, loc=0., scale=1.)
+        distribution_stddev = .87962566103423978
+
+        std = np.sqrt(1. / fan_in) / distribution_stddev
         a, b = -2. * std, 2. * std
 
         u = nn.init.trunc_normal_(module.w.data, std=std, a=a, b=b)