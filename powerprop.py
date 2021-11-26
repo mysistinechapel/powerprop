74cddb3b1818b8c5cc11d4f9f69dfd2324fa6a47
Jack Casey <25445014+jjkc33@users.noreply.github.com>
2021-11-26 14:37:32 -0800

    ensure mnist training script doesnt break

--- powerprop.py
+++ powerprop.py
@@ -1,18 +1,16 @@
 import torch
-
-from torchvision.datasets import MNIST, CIFAR10
-
-from torchvision.transforms import ToTensor
 from torch.utils.data import DataLoader, TensorDataset
 
 from utils import utils as uu
 from utils.pp_modules import MLP
+from utils.loading import load_data
 
 
 model_seed = 0
 torch.manual_seed(model_seed)
 
 # Training Configuration
+dataset = 'MNIST'
 alphas = [1.0, 2.0, 3.0, 4.0, 5.0]
 validation_size = 5000
 report_interval = 2500
@@ -23,29 +21,9 @@ num_train_steps = 5000
 learning_rate = 0.0025
 momentum = 0.9
 
-
-# Training setup
-train_data = MNIST(
-    root='./data/',
-    train=True,
-    download=True,
-    transform=ToTensor(),
-)
-test_data = MNIST(
-    root='./data/',
-    train=False,
-    download=True,
-    transform=ToTensor()
-)
-
-# reshape and normalize data
-train_data.data = uu.preprocess(train_data.data)
-test_data.data = uu.preprocess(test_data.data)
-
-train_x, train_y, valid_x, valid_y = uu.train_val_split(train_data, validation_size)
-
-test_x = test_data.data
-test_y = test_data.targets
+# load data
+train_x, train_y = load_data(dataset, train=True)
+test_x, test_y = load_data(dataset, train=False)
 
 dataloader = DataLoader(TensorDataset(train_x, train_y), batch_size=train_batch_size, shuffle=True)
 
