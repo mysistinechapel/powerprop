486284632a05b86365d442549db1f893e5322c64
Jack Casey <25445014+jjkc33@users.noreply.github.com>
2021-12-01 19:14:24 -0800

    rewrite CNN module and add plain CNN for reference

--- utils/pp_modules.py
+++ utils/pp_modules.py
@@ -102,60 +102,95 @@ class CNN(nn.Module):
     def __init__(self, alpha, in_channels=3, out_channels=(64, 128, 256)):
         super(CNN, self).__init__()
         self.alpha = alpha
-        self.conv_layers = nn.ModuleDict()
-        self.conv_norms = nn.ModuleList()
+        self.conv_layers = nn.ModuleList()
         self.fc_layers = nn.ModuleList()
-        self.fc_norms = nn.ModuleList()
+        self.batch_norms = nn.ModuleList()
 
         # create convolutional layers
-        for i, channels in enumerate(out_channels):
-            self.conv_layers[f'ConvLayer{2*i + 1}'] = PowerPropConv(
-                alpha=alpha, in_channels=in_channels, out_channels=channels
+        for channels in out_channels:
+            self.conv_layers.append(
+                PowerPropConv(alpha=alpha, in_channels=in_channels, out_channels=channels)
             )
-            self.conv_layers[f'ConvLayer{2*i + 2}'] = PowerPropConv(
-                alpha=alpha, in_channels=channels, out_channels=channels
+            self.conv_layers.append(
+                PowerPropConv(alpha=alpha, in_channels=channels, out_channels=channels)
             )
-            self.conv_norms.extend([
+            self.batch_norms.extend([
                 nn.BatchNorm2d(channels), nn.BatchNorm2d(channels)
             ])
             in_channels = channels
 
         # create FC layers
         in_features = 4096
-        for i, features in enumerate((256, 256, 10)):
+        for features in (256, 256, 10):
             self.fc_layers.append(
                 PowerPropLinear(alpha=alpha, in_features=in_features, out_features=features)
             )
-            if i < 2:
-                self.fc_norms.append(nn.BatchNorm1d(features))
             in_features = features
 
+        self.num_fcs = len(self.fc_layers)
+        self.num_convs = len(self.conv_layers)
+
     def get_weights(self):
-        weights = [layer.get_weights() for layer in self.conv_layers.values()]
+        weights = [layer.get_weights() for layer in self.conv_layers]
         weights.extend([layer.get_weights() for layer in self.fc_layers])
         return weights
 
     def forward(self, inputs, masks=None):
-        for i in range(len(self.conv_layers)):
-            layer = self.conv_layers[f'ConvLayer{i + 1}']
-            if masks is not None:
-                inputs = layer(inputs, masks[i])
-            else:
-                inputs = layer(inputs)
+        if masks is None:
+            masks = [None] * (self.num_convs + self.num_fcs)
 
-            inputs = F.relu(self.conv_norms[i](inputs))
+        for i, layer in enumerate(self.conv_layers):
+            inputs = layer(inputs, masks[i])
+            inputs = F.relu(self.batch_norms[i](inputs))
             if i % 2 == 1:  # pooling after each pair of conv layers
                 inputs = F.max_pool2d(inputs, kernel_size=2, stride=2)
 
         inputs = torch.flatten(inputs, start_dim=1)
-        num_fcs = len(self.fc_layers)
         for i, layer in enumerate(self.fc_layers):
-            if masks is not None:
-                inputs = layer(inputs, masks[i])
-            else:
-                inputs = layer(inputs)
-
-            if i < (num_fcs - 1):
-                inputs = F.relu(self.fc_norms[i](inputs))
+            inputs = layer(inputs, masks[i + self.num_convs])
+            if i < (self.num_fcs - 1):
+                inputs = F.relu(inputs)
 
         return inputs
+
+
+class PlainCNN(nn.Module):
+    """A convolutional neural network module."""
+    def __init__(self, in_channels=3, out_channels=(64, 128, 256)):
+        super(PlainCNN, self).__init__()
+        self.network = nn.Sequential()
+        # create convolutional layers
+        for i, channels in enumerate(out_channels):
+            self.make_block(in_channels, channels, 2 * i + 1)
+            self.make_block(channels, channels, 2 * i + 2)
+            self.network.add_module(
+                f'MaxPool{i + 1}',
+                nn.MaxPool2d(kernel_size=2, stride=2)
+            )
+            in_channels = channels
+
+        self.network.add_module('Flatten', nn.Flatten(start_dim=1))
+        # create FC layers
+        in_features = 4096
+        for i, features in enumerate([256, 256, 10]):
+            self.network.add_module(
+                f'Linear{i + 1}',
+                nn.Linear(in_features=in_features, out_features=features)
+            )
+            if i < 2:
+                self.network.add_module(
+                    f'ReLU{i + 7}',
+                    nn.ReLU()
+                )
+            in_features = features
+
+    def make_block(self, in_channels, channels, idx):
+        self.network.add_module(
+            f'Conv{idx}',
+            nn.Conv2d(in_channels=in_channels, out_channels=channels, kernel_size=(3, 3), stride=(1, 1), padding=1)
+        )
+        self.network.add_module(f'BatchNorm{idx}', nn.BatchNorm2d(channels))
+        self.network.add_module(f'ReLU{idx}', nn.ReLU())
+
+    def forward(self, inputs):
+        return self.network(inputs)
